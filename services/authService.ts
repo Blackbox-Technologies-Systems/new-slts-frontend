4b167d9ba5f7068d30642aa3d7c97b2a04d33eb0
Khadijat Abdullahi <khaptainkhad@gmail.com>
2026-04-21 16:26:05 +0100

    feat(auth): Add 2FA and Password Reset Methods
    -   Change NEXT_PUBLIC_APP_NAME from BBStarterPack
        to SLTS - Strict Liability Traffic System

--- services/authService.ts
+++ services/authService.ts
@@ -1,21 +1,48 @@
 import apiClient from "@/lib/apiClient";
-import type { AuthResponse, LoginCredentials, RegisterCredentials } from "@/types";
+import type { AuthResponse, LoginCredentials, RegisterCredentials, User } from "@/types";
 import { API_ENDPOINTS } from "@/constants";
 
 export const authService = {
-  async login(credentials: LoginCredentials): Promise<AuthResponse> {
-    const { data } = await apiClient.post<AuthResponse>(
-      API_ENDPOINTS.AUTH.LOGIN,
-      credentials
-    );
+  async loginWithCredentials(
+    credentials: LoginCredentials
+  ): Promise<{ message: string }> {
+    const { data } = await apiClient.post(API_ENDPOINTS.AUTH.LOGIN, credentials)
     return data;
   },
 
-  async register(credentials: RegisterCredentials): Promise<AuthResponse> {
-    const { data } = await apiClient.post<AuthResponse>(
-      API_ENDPOINTS.AUTH.REGISTER,
-      credentials
-    );
+  async verifyLoginOtp(payload: {
+    email: string
+    otp: string
+  }): Promise<{
+    token: string;
+    user: User
+  }> {
+    const { data } = await apiClient.post(API_ENDPOINTS.AUTH.VERIFYOTP, payload)
+    return data;
+  },
+
+  async forgotPassword(email: string): Promise<{ message: string }> {
+    const { data } = await apiClient.post(API_ENDPOINTS.AUTH.FORGOTPASSSWORD, { email })
+    return data;
+  },
+
+  async verifyForgotPasswordOtp(payload: {
+    email: string;
+    otp: string;
+  }): Promise<{
+    message: string;
+    reset_token: string
+  }> {
+    const { data } = await apiClient.post(API_ENDPOINTS.AUTH.VERIFYFORGOTPASSWORD, payload)
+    return data;
+  },
+
+  async setForgotPassword(payload: {
+    reset_token: string
+    password: string
+    password_confirmation: string
+  }): Promise<{ message: string }> {
+    const { data } = await apiClient.post("/auth/set-forgot-password", payload)
     return data;
   },
 
@@ -27,15 +54,4 @@ export const authService = {
     }
   },
 
-  async getMe(): Promise<AuthResponse["user"]> {
-    const { data } = await apiClient.get(API_ENDPOINTS.AUTH.ME);
-    return data;
-  },
-
-  async refreshToken(refreshToken: string): Promise<{ token: string }> {
-    const { data } = await apiClient.post(API_ENDPOINTS.AUTH.REFRESH, {
-      refreshToken,
-    });
-    return data;
-  },
 };
